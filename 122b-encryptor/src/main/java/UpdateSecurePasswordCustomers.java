8c00d3adcfc36eb767f0f39cbffc088c5a064c77
benjaw5 <93418521+benjaw5@users.noreply.github.com>
2023-12-08 22:06:24 -0800

    master url

--- 122b-encryptor/src/main/java/UpdateSecurePasswordCustomers.java
+++ 122b-encryptor/src/main/java/UpdateSecurePasswordCustomers.java
@@ -24,7 +24,7 @@ public class UpdateSecurePasswordCustomers {
 
         String loginUser = "mytestuser";
         String loginPasswd = "My6$Password";
-        String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+        String loginUrl = "jdbc:mysql://172.31.3.64:3306/moviedb";
 
         Class.forName("com.mysql.jdbc.Driver").newInstance();
         Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
