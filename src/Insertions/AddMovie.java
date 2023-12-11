9b841ac293cd26f902e32e17c257c95f17094b48
Howard You <cyou6@uci.edu>
2023-12-10 23:29:08 -0800

    logs and img

--- src/Insertions/AddMovie.java
+++ src/Insertions/AddMovie.java
@@ -27,7 +27,7 @@ public class AddMovie extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/master");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
         } catch (NamingException e) {
             e.printStackTrace();
         }
--- time-log/log_processing.py
+++ time-log/log_processing.py
@@ -1,22 +1,37 @@
+import sys
 
-
-with open("timeTJ.txt", "r") as file:
+def processTJ(filepaths):
     totalTime = 0
     length = 0
-    for line in file:
-        totalTime += float(line.strip())
-        length += 1
-    totalTime /= 1000000000
-    print(f"TJ total time = {totalTime} seconds", end=" ")
-    print(f"TJ average time = {totalTime/length} seconds")
+    for fp in filepaths:
+        with open(fp, "r") as file:
+            for line in file:
+                totalTime += float(line.strip())
+                length += 1
+    totalTime /= 1000000
+    print(f"TJ total time = {totalTime} ms", end=" ")
+    print(f"TJ average time = {totalTime/length} ms")
 
 
-with open("timeTS.txt", "r") as file:
+def processTS(filepaths):
     totalTime = 0
     length = 0
-    for line in file:
-        totalTime += float(line.strip())
-        length += 1
-    totalTime /= 1000000000
-    print(f"TS total time = {totalTime} seconds", end=" ")
-    print(f"TS average time = {totalTime/length} seconds")
+    for fp in filepaths:
+        with open(fp, "r") as file:
+            for line in file:
+                totalTime += float(line.strip())
+                length += 1
+    totalTime /= 1000000
+    print(f"TS total time = {totalTime} ms", end=" ")
+    print(f"TS average time = {totalTime/length} ms")
+    
+ 
+if __name__ == "__main__":
+    filepathsTJ = [sys.argv[1]]
+    filepathsTS = [sys.argv[2]]
+    if len(sys.argv) > 3:
+        filepathsTJ.append(sys.argv[3])
+        filepathsTS.append(sys.argv[4])
+
+    processTJ(filepathsTJ)
+    processTS(filepathsTS)
